34458b9ee35074c241cf0e9cdec9d807bc206b7c
glombort <george.lambert99@gmail.com>
2022-06-08 16:30:33 +0100

    Added Film inputabliity
    Co-Authored-By: Hallie  <90645969+vasystus@users.noreply.github.com>

--- really-bad-movie/src/App.jsx
+++ really-bad-movie/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import React from "react"
+import { useState } from "react";
+import React from "react";
 //import logo from './game.jsx'
-import './App.css'
+import "./App.css";
 
 import Lives from "./components/Lives.jsx";
 import Game from "./components/Game.jsx";
@@ -9,35 +9,39 @@ import Game from "./components/Game.jsx";
 function App() {
   const [name, setName] = React.useState(null);
   const [lives, setLives] = React.useState(5);
-  const [correct, setCorrect] = React.useState(1);
+  const [correct, setCorrect] = React.useState(0);
 
   return (
     <div className="App">
       <header className="header">
         <h1>Badflix</h1>
-        <p className='rules'>Rules should be here</p>
+        <p className="rules">Rules should be here</p>
         <input
           type="search"
           name="typeusername"
           className="playerInput"
           aria-label="enter your username"
           required
-        /><br/>
+        />
+        <br />
         <button type="submit" className="button">
           Submit Player
         </button>
-        <p>
-          Hi, {name}!
-        </p>
-        <section className='lives'>
-          <Lives lives={lives}/>
+        <p>Hi, {name}!</p>
+        <section className="lives">
+          <Lives lives={lives} />
         </section>
         <section className="gameArea">
-          <Game lives={lives} setLives={setLives} correct={correct} setCorrect={setCorrect}/>
+          <Game
+            lives={lives}
+            setLives={setLives}
+            correct={correct}
+            setCorrect={setCorrect}
+          />
         </section>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- really-bad-movie/src/components/Game.jsx
+++ really-bad-movie/src/components/Game.jsx
@@ -4,55 +4,93 @@ const MOVIE_URL = "https://api.themoviedb.org/3/";
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 const API_KEY = "87337df5190b4447f246a4872658a898";
 
-function fetchFilm(filmNum, page) {
-  const DISCOVER_URL = MOVIE_URL + `discover/movie?api_key=${API_KEY}&include_video=false&page=${page}`
+function fetchFilm(page) {
+  const DISCOVER_URL =
+    MOVIE_URL +
+    `discover/movie?api_key=${API_KEY}&include_video=false&page=${page}`;
   return fetch(DISCOVER_URL).then((res) => {
     if (!res.ok) throw new Error("HTTP error");
-    return res.json()
-  })
+    return res.json();
+  });
 }
 
 function FilmTile(props) {
   const [filmData, setFilmData] = React.useState(null);
-  
-  
-  React.useEffect(() => {
-    fetchFilm(props.id, props.correct)
-    .then((json) => {
-      const film = json.results[props.id]
-      setFilmData(film)
-      props.setRating(film.popularity)
-    })
-  }, [])
 
-  if(!filmData) return <div>Loading...</div>
+  React.useEffect(() => {
+    fetchFilm(props.correct + 1).then((json) => {
+      const film = json.results[props.tileId];
+      setFilmData(film);
+      props.setRating(film.popularity);
+    });
+  }, [props]);
+
+  if (!filmData) return <div>Loading...</div>;
   return (
-    <div>Tile {filmData.original_title}
-    <img src={IMAGE_URL + filmData.poster_path}/>
-    
-    </div>
-  )
+    <>
+      <label htmlFor={props.tileId}>
+        Tile {filmData.original_title}
+        <input
+          id={props.tileId}
+          type="image"
+          name={`submit_${props.tileId}`}
+          value={props.tileId}
+          alt={`Poster of ${filmData.original_title}`}
+          src={IMAGE_URL + filmData.poster_path}
+        />
+      </label>
+      <div>rating= {props.rating}</div>
+    </>
+  );
+  //rating={</filmData.ring}atdiv>  ;
 }
 
 export default function Game(props) {
-  const [rating1, setRating1] = React.useState(0);
-  const [rating2, setRating2] = React.useState(0);
+  const [rating1, setRating1] = React.useState(null);
+  const [rating2, setRating2] = React.useState(null);
+  const [higherRating, setHigherRating] = React.useState(null);
+
+  React.useEffect(() => {
+    return rating1 > rating2 ? setHigherRating(1) : setHigherRating(2);
+  }, [rating1, rating2]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(props.lives, props.correct);
+    const filmPick = event.nativeEvent.submitter.value;
+    if (parseInt(filmPick) !== parseInt(higherRating)) {
+      props.setLives(props.lives - 1);
+    } else {
+      props.setCorrect(props.correct + 1);
+    }
+  }
+
+  //if (!rating1 | !rating2) return <div>Loading...</div>;
   return (
-    <form>
-      <FilmTile id={1} correct={props.correct} rating={rating1} setRating={setRating1}/>
-      <FilmTile id={2} correct={props.correct} rating={rating2} setRating={setRating2}/>
+    <form onSubmit={(event) => handleSubmit(event)}>
+      <FilmTile
+        tileId={1}
+        correct={props.correct}
+        rating={rating1}
+        setRating={setRating1}
+      />
+      <FilmTile
+        tileId={2}
+        correct={props.correct}
+        rating={rating2}
+        setRating={setRating2}
+      />
     </form>
-  )
-};
+  );
+}
 
 //flipcards
 //  <div className ="flip_card">
 //     <div className="flip_card_inner">
 //         <div calssName="flip_card_front">
 //              Tile {filmData.original_title}
-//             <img src={IMAGE_URL + filmData.poster_path} alt="poster" style="width:300px;height:300px;"/> 
+//             <img src={IMAGE_URL + filmData.poster_path} alt="poster"/>
 //         </div>
 //         <div className="flip_card_back">RATING</div>
 //     </div>
-// </div>  
-
+// </div>
