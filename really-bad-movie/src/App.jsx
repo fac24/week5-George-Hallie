03446574db70cd44bcbc30041738e60f9efb1db1
vasystus <artgalla8@gmail.com>
2022-06-09 15:24:38 +0100

    user's greeting shows up on submitin gthe form

--- really-bad-movie/src/App.jsx
+++ really-bad-movie/src/App.jsx
@@ -6,29 +6,60 @@ import Lives from "./components/Lives.jsx";
 import Game from "./components/Game.jsx";
 
 function App() {
-  const [name, setName] = React.useState(null);
+  const [name, setName] = React.useState('');
   const [lives, setLives] = React.useState(5);
   const [correct, setCorrect] = React.useState(0);
   const [guesses, setGuesses] = React.useState(0);
+  
+  
+  // React.useEffect(() => {
+  //   document.title = `Hello ${name}`;
+
+  // });
+
+  const updateName = (event) => {
+    setName(event.target.value);
+  }
+
+  const showName = event => {
+    event.preventDefault();
+    document.getElementById("showtime").innerHTML = `Hello, ${name}!`;
+    document.getElementById("form").style.display="none";
+  };
+
+
+ 
 
   return (
     <div className="App">
-      <header className="header">
-        <h1>Badflix</h1>
+      <header >
+        <h1 className="title">Badflix</h1>
         <p className="rules">Rules should be here</p>
-        <input
-          type="search"
-          name="typeusername"
-          className="playerInput"
-          aria-label="enter your username"
-          required
-          onChange={(event) => setName(event.target.value)}
-        />
+        
+        <section className = "hello_user">
+          <form id="form">
+            <label data-for="name">Please enter your name: </label>
+               <input
+               type="text"
+               name="typename"
+               className="playerInput"
+               aria-label="enter your username"
+               value={name}
+               onChange={updateName}
+               required
+               />
+          <button onClick={showName}>Submit</button> 
 
-        <p>Hi, {name}!</p>
+          
+          </form>
+          <div className="greeting"  id="showtime"></div>
+          
+        </section>
+
         <section className="lives">
           <Lives lives={lives} correct={correct} />
         </section>
+
         <section className="gameArea">
           <Game
             lives={lives}
