1e29eb938f11c0eca154c5312d5693a53b3e319d
glombort <george.lambert99@gmail.com>
2022-06-09 12:51:05 +0100

    Restart button and more random
    Added a restart button that appears on losing all lives
    
    Films now more random than before
    
    Co-Authored-By: Hallie  <90645969+vasystus@users.noreply.github.com>

--- really-bad-movie/src/components/Game.jsx
+++ really-bad-movie/src/components/Game.jsx
@@ -5,7 +5,9 @@ export default function Game(props) {
   const [rating1, setRating1] = React.useState(null);
   const [rating2, setRating2] = React.useState(null);
   const [higherRating, setHigherRating] = React.useState(null);
-
+  const [randomIndex, setRandomIndex] = React.useState(
+    Math.floor(Math.random() * 10 + 1)
+  );
   React.useEffect(() => {
     return rating1 > rating2 ? setHigherRating(1) : setHigherRating(2);
   }, [rating1, rating2]);
@@ -14,7 +16,6 @@ export default function Game(props) {
     event.preventDefault();
     console.log(props.lives, props.correct);
     const filmPick = event.nativeEvent.submitter.value;
-
     if (parseInt(filmPick) !== parseInt(higherRating)) {
       props.setLives(props.lives - 1);
     } else {
@@ -27,13 +28,27 @@ export default function Game(props) {
       <button
         onClick={() => {
           props.setGuesses(props.guesses + 1);
+          setRandomIndex(Math.floor(Math.random() * 10 + 1));
         }}
       >
         Next Film
       </button>
     ) : null;
-  if (props.lives === 0) return null;
-  //if (!rating1 | !rating2) return <div>Loading...</div>;
+
+  const restartButton = (
+    <button
+      onClick={() => {
+        props.setGuesses(0);
+        props.setLives(5);
+        props.setCorrect(0);
+      }}
+    >
+      RESTART GAME
+    </button>
+  );
+
+  if (props.lives === 0) return restartButton;
+  //if (rating1 === null || rating2 === null) return <div>Loading...</div>;
   return (
     <>
       <form onSubmit={(event) => handleSubmit(event)}>
@@ -45,6 +60,7 @@ export default function Game(props) {
             guesses={props.guesses}
             rating={rating1}
             setRating={setRating1}
+            randomIndex={randomIndex}
           />
           <FilmTile
             tileId={2}
@@ -53,6 +69,7 @@ export default function Game(props) {
             correct={props.correct}
             rating={rating2}
             setRating={setRating2}
+            randomIndex={randomIndex}
           />
         </div>
       </form>
