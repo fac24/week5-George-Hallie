d831352e9ddabdff0111cf6634da316932c7ffe4
glombort <george.lambert99@gmail.com>
2022-06-09 12:50:23 +0100

    Added Rules
    Quick add of rules could probably be made better
    
    Co-Authored-By: Hallie  <90645969+vasystus@users.noreply.github.com>

---
+++ really-bad-movie/src/components/Rules.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Rules(props) {
+  const [open, setOpen] = React.useState(false);
+  function toggleOpen() {
+    setOpen(!open);
+  }
+  const rules = !open ? null : (
+    <div>
+      <ul>
+        <li>You have a choice between 2 films</li>
+        <li>
+          Pick the AwFilm (based on current popularity according to tmdb users)
+        </li>
+        <li>You have 5 Lives</li>
+        <li>Have a Filmtastic time</li>
+      </ul>
+    </div>
+  );
+  return (
+    <div className="rules">
+      <button onClick={toggleOpen}>Rules</button>;{rules}
+    </div>
+  );
+}
